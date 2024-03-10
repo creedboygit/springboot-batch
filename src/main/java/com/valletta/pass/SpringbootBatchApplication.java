50a66045029aa76dd9dd2760c002f1c3b5802620
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-10 23:48:43 +0900

    batch first test

--- src/main/java/com/valletta/pass/SpringbootBatchApplication.java
+++ src/main/java/com/valletta/pass/SpringbootBatchApplication.java
@@ -1,10 +1,35 @@
 package com.valletta.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootApplication
 public class SpringbootBatchApplication {
+
+	@Bean
+	public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("passStep", jobRepository)
+			.tasklet((stepContribution, chunkContext) -> {
+				System.out.println("Execute PassStep");
+				return RepeatStatus.FINISHED;
+			}, transactionManager).build();
+	}
+
+	@Bean
+	public Job passJob(JobRepository jobRepository, Step passStep) {
+		return new JobBuilder("passJob", jobRepository)
+			.start(passStep)
+			.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBatchApplication.class, args);
