29eb6be20f9973a7174a0dd25f3f2fc1d325d7ac
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-11 23:44:04 +0900

    entity, repository 설정

--- src/main/java/com/valletta/pass/repository/packaze/PackageRepository.java
+++ src/main/java/com/valletta/pass/repository/packaze/PackageRepository.java
@@ -1,5 +1,23 @@
 package com.valletta.pass.repository.packaze;
 
-public class PackageRepository {
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
+
+    List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update PackageEntity p " +
+        "set p.count = :count, " +
+        "p.period = :period " +
+        "where p.packageSeq = :packageSeq")
+    int updateCountAndPeriod(@Param("packageSeq") Integer packageSeq, @Param("count") Integer count, @Param("period") Integer period);
 }
