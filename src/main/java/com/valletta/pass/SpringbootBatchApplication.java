f9961a0854351bde3153805f86aee41c53905cb5
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-11 21:55:57 +0900

    spring batch test

--- src/main/java/com/valletta/pass/SpringbootBatchApplication.java
+++ src/main/java/com/valletta/pass/SpringbootBatchApplication.java
@@ -2,37 +2,57 @@ package com.valletta.pass;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringbootBatchApplication {
 
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	public SpringbootBatchApplication(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory) {
+		this.jobBuilderFactory = jobBuilderFactory;
+		this.stepBuilderFactory = stepBuilderFactory;
+	}
+	
 	@Bean
-	public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-		return new StepBuilder("passStep", jobRepository)
-			.tasklet((stepContribution, chunkContext) -> {
-				System.out.println("Execute PassStep");
-				return RepeatStatus.FINISHED;
-			}, transactionManager).build();
+	public Step passStep() {
+		return this.stepBuilderFactory.get("passStep")
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("Execute PassStep");
+					return RepeatStatus.FINISHED;
+				}
+			}).build();
 	}
 
 	@Bean
-	public Job passJob(JobRepository jobRepository, Step passStep) {
-		return new JobBuilder("passJob", jobRepository)
-			.start(passStep)
+	public Job passJob() {
+		return this.jobBuilderFactory.get("passJob")
+			.start(passStep())
 			.build();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBatchApplication.class, args);
 	}
-
 }
