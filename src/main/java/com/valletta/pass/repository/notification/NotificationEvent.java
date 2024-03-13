ec096922b7abb888a51d8754e55c7b7ab235f565
주현(백엔드개발1팀) <rich@hanssem.com>
2024-03-13 16:17:20 +0900

    entity, repository

---
+++ src/main/java/com/valletta/pass/repository/notification/NotificationEvent.java
@@ -0,0 +1,5 @@
+package com.valletta.pass.repository.notification;
+
+public enum NotificationEvent {
+    BEFORE_CLASS
+}
---
+++ src/main/java/com/valletta/pass/repository/notification/NotificationModelMapper.java
@@ -0,0 +1,9 @@
+package com.valletta.pass.repository.notification;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface NotificationModelMapper {
+
+}
