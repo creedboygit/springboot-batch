a175c19455146efb7f54189917f0fd2d03adcd03
주현(백엔드개발1팀) <rich@hanssem.com>
2024-03-14 10:47:18 +0900

    entity, repository

---
+++ src/main/java/com/valletta/pass/repository/statistics/StatisticsRepository.java
@@ -0,0 +1,6 @@
+package com.valletta.pass.repository.statistics;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StatisticsRepository extends JpaRepository<StatisticsEntity, Integer> {
+}
