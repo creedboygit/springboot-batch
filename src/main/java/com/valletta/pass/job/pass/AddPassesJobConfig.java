ecc02b7cf88f5c8cdc47e3cc81a5c77d6fee790d
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-15 18:44:19 +0900

    이용권 일괄 지급 구현

--- src/main/java/com/valletta/pass/job/pass/AddPassesJobConfig.java
+++ src/main/java/com/valletta/pass/job/pass/AddPassesJobConfig.java
@@ -1,8 +1,11 @@
 package com.valletta.pass.job.pass;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,4 +16,17 @@ public class AddPassesJobConfig {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final AddPassesTasklet addPassesTasklet;
+
+    @Bean
+    public Job addPassesJob() {
+        return this.jobBuilderFactory.get("addPassesJob")
+            .start(addPassesStep())
+            .build();
+    }
+
+    private Step addPassesStep() {
+        return this.stepBuilderFactory.get("addPassesStep")
+            .tasklet(addPassesTasklet)
+            .build();
+    }
 }
---
+++ src/test/java/com/valletta/pass/repository/pass/PassModelMapperTest.java
@@ -0,0 +1,8 @@
+package com.valletta.pass.repository.pass;
+
+import org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+
+public class PassModelMapperTest {
+
+}
