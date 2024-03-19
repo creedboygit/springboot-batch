6f60b0a45a33b4f01930d5d81793df603333c67e
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-19 22:13:55 +0900

    bulk-pass View 구현

--- src/main/java/com/valletta/pass/controller/admin/AdminViewController.java
+++ src/main/java/com/valletta/pass/controller/admin/AdminViewController.java
@@ -7,7 +7,6 @@ import com.valletta.pass.service.user.UserGroupMappingService;
 import com.valletta.pass.util.LocalDateTimeUtils;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
