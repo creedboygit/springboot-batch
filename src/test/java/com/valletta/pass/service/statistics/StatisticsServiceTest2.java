10f70859530fde0e27127db2b41411a87bc3a9d7
Ju Hyun(한샘몰백엔드개발팀) <polozh@naver.com>
2024-03-22 21:27:20 +0900

    statistics Test

--- src/test/java/com/valletta/pass/service/statistics/StatisticsServiceTest2.java
+++ src/test/java/com/valletta/pass/service/statistics/StatisticsServiceTest2.java
@@ -2,6 +2,7 @@ package com.valletta.pass.service.statistics;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -11,7 +12,10 @@ import com.valletta.pass.repository.statistics.AggregatedStatistics;
 import com.valletta.pass.repository.statistics.StatisticsRepository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,6 +30,52 @@ public class StatisticsServiceTest2 {
 
     @InjectMocks
     private StatisticsService statisticsService;
+
+    @Nested
+    @DisplayName("통계 데이터를 기반으로 차트 만들기")
+    class MakeChartData {
+
+        final LocalDateTime to = LocalDateTime.of(2024, 3, 24, 0, 0);
+
+        @DisplayName("통계 데이터가 있을 때")
+        @Test
+        void makeChartData_when_hasStatistics() {
+
+            List<AggregatedStatistics> statisticsList = List.of(
+                new AggregatedStatistics(to.minusDays(1), 15, 10, 5),
+                new AggregatedStatistics(to, 20, 3, 40)
+            );
+
+            // when
+            when(statisticsRepository.findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to))).thenReturn(statisticsList);
+            final ChartData chartData = statisticsService.makeChartData(to);
+
+            // then
+            verify(statisticsRepository, times(1)).findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to));
+
+            assertNotNull(chartData);
+            assertEquals(new ArrayList<>(List.of("03-23", "03-24")), chartData.getLabels());
+            assertEquals(new ArrayList<>(List.of(10L, 3L)), chartData.getAttendedCounts());
+            assertEquals(new ArrayList<>(List.of(5L, 40L)), chartData.getCanceledCounts());
+        }
+
+        @DisplayName("통계 데이터가 없을 때")
+        @Test
+        void makeChartData_when_notHasStatistics() {
+
+            // when
+            when(statisticsRepository.findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to))).thenReturn(Collections.emptyList());
+            final ChartData chartData = statisticsService.makeChartData(to);
+
+            // then
+            verify(statisticsRepository, times(1)).findByStatisticsAtBetweenAndGroupBy(eq(to.minusDays(10)), eq(to));
+
+            assertNotNull(chartData);
+            assertTrue(chartData.getLabels().isEmpty());
+            assertTrue(chartData.getAttendedCounts().isEmpty());
+            assertTrue(chartData.getCanceledCounts().isEmpty());
+        }
+    }
 
     @Test
     public void test_makeChartData() {
